a520979b4707b13dd552f38722223d171e4d9eeb
Paul Redmond <paulrredmond@gmail.com>
2013-02-19 16:16:22 -0700

    Add manifest and basic readme.

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install PhpStorm.app into /Applications.
+#
+# Examples
+#
+#   include sequelpro
+class phpstorm {
+  package { 'PhpStorm':
+    source   => 'http://download.jetbrains.com/webide/PhpStorm-5.0.4.dmg',
+    provider => 'appdmg'
+  }
+}
