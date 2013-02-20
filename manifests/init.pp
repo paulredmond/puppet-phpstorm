83ae5761fcca809753e542686e01ce6103c26a62
Paul Redmond <paulrredmond@gmail.com>
2013-02-19 17:18:51 -0700

    Trying to append a line to gitignore

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,15 @@
 #
 #   include phpstorm
 class phpstorm {
+
+  require git::config
+
   package { 'PhpStorm':
     source   => 'http://download.jetbrains.com/webide/PhpStorm-5.0.4.dmg',
     provider => 'appdmg'
+  }
+
+  exec { "echo atlassian-ide-plugin.xml >> ${git::config::configdir}/gitignore"
+    require => File["${git::config::configdir}/gitignore"]
   }
 }
