390629f373abbc0ae7fccf6f993cc2aa04e955eb
Paul Redmond <paulrredmond@gmail.com>
2013-03-02 21:25:29 -0700

    Dropping gitignore exec.

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,8 +11,4 @@ class phpstorm {
     source   => 'http://download.jetbrains.com/webide/PhpStorm-5.0.4.dmg',
     provider => 'appdmg'
   }
-
-  exec { "echo atlassian-ide-plugin.xml >> ${git::config::configdir}/gitignore":
-    require => File["${git::config::configdir}/gitignore"]
-  }
 }
